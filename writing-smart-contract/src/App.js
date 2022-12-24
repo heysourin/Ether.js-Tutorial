f826ef7f374aff38c4d9b8baf61db2aff1b43e85
heysourin <sourin07@gmail.com>
2022-12-24 20:20:06 +0530

    Writing Contract, Sending Ethers

---
+++ writing-smart-contract/src/App.js
@@ -0,0 +1,126 @@
+import "./App.css";
+import { useEffect } from "react";
+const { ethers } = require("ethers");
+function App() {
+  const contractAddress = "0x49ecf06c3ae01a27CcE4F713f2eFEeaF9d4b7170";
+
+
+  useEffect(() => {
+    const contractABI = [
+      {
+        inputs: [
+          {
+            internalType: "address",
+            name: "_address",
+            type: "address",
+          },
+        ],
+        name: "accountBalance",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "contractBalance",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "getValue",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "name",
+        outputs: [
+          {
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "sendEthContract",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "address",
+            name: "_user",
+            type: "address",
+          },
+        ],
+        name: "sendEthUsers",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "_num",
+            type: "uint256",
+          },
+        ],
+        name: "setValue",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+    const writeContract = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      //TODO: Writing Contract
+      // await contract.setValue(4);
+
+      //TODO: Sending Ether from User to Contract
+      // await contract.sendEthContract({value:ethers.utils.parseEther("0.0001")});
+
+      //TODO: Sending Ether from Contract to User
+      await contract.sendEthUsers("0x2411303b59444ac2f3C434D8a77BF41f1606AB46",{value:ethers.utils.parseEther("0.0001")});
+    };
+    writeContract();
+  });
+  return <div className="App"></div>;
+}
+
+export default App;
